f1bc093fe969b8901a8f987c9afc61a08849ef21
chethan25 <chethan25.bpac@gmail.com>
2023-05-07 19:59:11 +0530

    ternary operator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const age = 19;
+  const isGreen = true;
+  const isPresent = null;
+
+  return (
+    <div className="App">
+      {age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      <h1 style={{ color: isGreen ? 'green' : 'red' }}> This has color</h1>
+      {isGreen && <button>Click Me!</button>}
+      {isPresent ?? <h2>I am present</h2>}
+    </div>
+  );
 }
 
 export default App;
