f99802dd323d274365ac5bb02ffa25a45ec325dc
chethan25 <chethan25.bpac@gmail.com>
2023-05-07 20:24:18 +0530

    exercise

--- src/App.js
+++ src/App.js
@@ -16,11 +16,23 @@ function App() {
       age: 45,
     },
   ];
+  const planets = [
+    { name: 'Mars', isGasPlanet: false },
+    { name: 'Earth', isGasPlanet: false },
+    { name: 'Jupiter', isGasPlanet: true },
+    { name: 'Venus', isGasPlanet: false },
+    { name: 'Neptune', isGasPlanet: true },
+    { name: 'Uranus', isGasPlanet: true },
+  ];
+
   return (
     <div className="App">
       {users.map((user, key) => (
         <User name={user.name} age={user.age} key={key} />
       ))}
+      {planets.map(
+        (planet, key) => !planet.isGasPlanet && <h2 key={key}>{planet.name}</h2>
+      )}
     </div>
   );
 }
