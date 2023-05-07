9b348cd526f43ff59d44e0f1c608c74688f0f5a6
chethan25 <chethan25.bpac@gmail.com>
2023-05-07 20:18:01 +0530

    rendering lists

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.css';
+import { User } from './User';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const users = [
+    {
+      name: 'Pedro',
+      age: 21,
+    },
+    {
+      name: 'Jake',
+      age: 25,
+    },
+    {
+      name: 'Jessica',
+      age: 45,
+    },
+  ];
+  return (
+    <div className="App">
+      {users.map((user, key) => (
+        <User name={user.name} age={user.age} key={key} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
