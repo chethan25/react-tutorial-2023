a03be441b193110f685ce1a49221d2ca1b156b11
chethan25 <chethan25.bpac@gmail.com>
2023-05-07 19:22:55 +0530

    ca

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <User name="chethan" age="21" email="chethan@gmail.com" />
+      <User name="chethan" age="21" email="chethan@gmail.com" />
+      <User name="chethan" age="21" email="chethan@gmail.com" />
+    </div>
+  );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1>{props.name}</h1>
+      <h1>{props.age}</h1>
+      <h1>{props.email}</h1>
+    </div>
+  );
+};
 
 export default App;
