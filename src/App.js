7bcce49e2b86929d86b6ac7360f21a30b05fd36b
chethan25 <chethan25.bpac@gmail.com>
2023-05-09 08:55:57 +0530

    useState exercise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  let [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Set to Zero</button>
+      <h1>{count}</h1>
+    </div>
+  );
 }
 
 export default App;
