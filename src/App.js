8ad535875d8912149a917aeec75faa0aee800f0b
chethan25 <chethan25.bpac@gmail.com>
2023-05-22 18:25:50 +0530

    predict age

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 
 function App() {
   const [catFact, setCatFact] = useState();
+  const [name, setName] = useState('');
+  const [predictedAge, setPredictedAge] = useState(null);
 
   useEffect(() => {
     fetchCatFact();
@@ -16,10 +18,27 @@ function App() {
       .then((res) => setCatFact(res.data.fact));
   };
 
+  const fetchData = () => {
+    axios
+      .get(`https://api.agify.io/?name=${name}`)
+      .then((res) => setPredictedAge(res.data));
+  };
+
   return (
     <div className="App">
       <button onClick={fetchCatFact}>Generate Cat Fact</button>
       <p>{catFact}</p>
+
+      <input
+        type="text"
+        onChange={(event) => {
+          setName(event.target.value);
+        }}
+      />
+      <button onClick={fetchData}>Predict Age</button>
+      <p>Name: {predictedAge?.name}</p>
+      <p>Predicted Age: {predictedAge?.age}</p>
+      <p>Count: {predictedAge?.count}</p>
     </div>
   );
 }
