38a712abb21cdc36583e2c02d064182e1656606c
chethan25 <chethan25.bpac@gmail.com>
2023-05-22 18:39:10 +0530

    excuser

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [excuse, setExcuse] = useState('');
+
+  const fetchExcuse = (category) => {
+    axios
+      .get(`https://excuser-three.vercel.app/v1/excuse/${category}`)
+      .then((res) => setExcuse(res.data[0].excuse));
+  };
+  return (
+    <div className="App">
+      <h1>Generate an Excuse</h1>
+      <button onClick={() => fetchExcuse('party')}>Party</button>
+      <button onClick={() => fetchExcuse('family')}>Family</button>
+      <button onClick={() => fetchExcuse('office')}>Office</button>
+
+      <p>{excuse}</p>
+    </div>
+  );
 }
 
 export default App;
