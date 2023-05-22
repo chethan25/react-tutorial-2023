9404d46006e9712b517c6571488c09a2e45268c7
chethan25 <chethan25.bpac@gmail.com>
2023-05-22 14:37:05 +0530

    built todo list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Job salary={90000} position="Senior SDE" company="Amazon" />
-      <Job salary={230000} position="Junior SDE" company="Google" />
-      <Job salary={120000} position="Project Manager" company="Netflix" />
-    </div>
-  );
+  return <div className="App">Hello World</div>;
 }
-
-const Job = ({ salary, position, company }) => {
-  return (
-    <ul>
-      <li>Company: {company}</li>
-      <li>Position: {position}</li>
-      <li>Salary: {salary}</li>
-    </ul>
-  );
-};
 
 export default App;
