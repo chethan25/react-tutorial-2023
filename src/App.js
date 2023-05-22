49e14a7cd233807cec1c08fdf68fb4759b0936df
chethan25 <chethan25.bpac@gmail.com>
2023-05-22 19:02:10 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import './App.css';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import Contact from './pages/Contact';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <div className="App">
+      <Router>
+        <div className="nav-bar">
+          <Link to="/">Home</Link>
+          <Link to="/menu">Menu</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<PageNotFound />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
