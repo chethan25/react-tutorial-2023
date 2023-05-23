56bb71003bd6e6b5d4c7936fe6c506be5e23fcc4
chethan25 <chethan25.bpac@gmail.com>
2023-05-23 14:15:29 +0530

    useQuery

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import './App.css';
@@ -7,21 +8,31 @@ import Contact from './pages/Contact';
 import PageNotFound from './pages/PageNotFound';
 
 function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: true,
+      },
+    },
+  });
+
   return (
     <div className="App">
-      <Router>
-        <div className="nav-bar">
-          <Link to="/">Home</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/contact">Contact</Link>
-        </div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<PageNotFound />}></Route>
-        </Routes>
-      </Router>
+      <QueryClientProvider client={client}>
+        <Router>
+          <div className="nav-bar">
+            <Link to="/">Home</Link>
+            <Link to="/profile">Profile</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<PageNotFound />}></Route>
+          </Routes>
+        </Router>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,31 @@
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+
 function Home() {
-  return <div>Home</div>;
+  const {
+    data: catData,
+    isLoading,
+    isError,
+    refetch,
+  } = useQuery(['cat'], () =>
+    axios.get('https://catfact.ninja/fact').then((res) => res.data)
+  );
+
+  if (isError) {
+    return <h1>Sorry, there was an error</h1>;
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  return (
+    <div>
+      Home
+      <p>{catData?.fact}</p>
+      <button onClick={refetch}>Update Fact</button>
+    </div>
+  );
 }
 
 export default Home;
